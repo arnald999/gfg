b7faaec0c03a6e644eeea8af52902229e1c40673
arnald999 <arnaldmib@gmail.com>
2019-07-12 23:01:09 +0530

    Graph updated

---
+++ Graphs/graphEdgePropertyCheck/graphedge.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+void dfs(int u, int visited[], vector<int> AL[], int parent[])
+{
+	visited[u] = 1;
+	
+	vector<int> neighbors = AL[u]; 
+	for(int i=0; i<neighbors.size(); i++)
+	{
+		if(visited[neighbors[i]]==0)
+		{
+			parent[neighbors[i]] = u;
+			dfs(neighbors[i],visited,AL,parent);
+		}
+		else if(visited[neighbors[i]]==1)
+		{
+			if(neighbors[i]==parent[u])
+				cout<<"Bidirectional : "<<u<<"->"<<neighbors[i]<<" "<<neighbors[i]<<"->"<<u<<endl;	//Loop
+			else
+				cout<<"Back Edge :"<<u<<"->"<<neighbors[i]<<endl;		//Back Edge in DFS means CYCLE in DIRECTED graph
+		}
+		else if(visited[neighbors[i]]==2)
+		{
+			cout<<"Forward/Cross edge :"<<u<<"->"<<neighbors[i]<<endl;
+		}
+	}
+	visited[u] = 2;
+}
+
+int main()
+{
+	int nodes,edges,x,y,i;
+	cin>>nodes>>edges;
+	
+	vector<int> AL[nodes];
+	
+	for(i=0; i<edges; i++)
+	{
+		cin>>x>>y;
+		AL[x].push_back(y);
+	}
+	
+	int visited[nodes] = {0};	//0,1,2
+	int parent[nodes] = {-1};
+	int numCC=0;
+	
+	for(i=0; i<nodes; i++)
+	{
+		if(!visited[i])
+		{
+			numCC++;
+			dfs(i,visited,AL,parent);
+		}
+	}
+	cout<<"no of connected components : "<<numCC<<endl;
+	return 0;
+}
---
+++ Graphs/topsort/topsort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+void dfs(int u, bool visited[], vector<int> AL[], stack<int> &stk)
+{
+	if(visited[u])
+		return;
+	
+	//cout<<u<<" ";
+	visited[u] = true;
+	vector<int> neighbors = AL[u];
+	
+	for(int i=0; i<neighbors.size(); i++)
+	{
+		if(!visited[neighbors[i]])
+			dfs(neighbors[i],visited,AL,stk);
+	}
+	stk.push(u);
+}
+
+
+int main()
+{
+	int nodes,edges,x,y,i;
+	
+	cin>>nodes>>edges;
+	
+	
+	vector<int> AL[nodes];
+	
+	for(i=0; i<edges; i++)
+	{
+		cin>>x>>y;
+		AL[x].push_back(y);
+	}
+	
+	bool visited[nodes] = {false};
+	stack<int> stk;
+	
+	for(i=0; i<nodes; i++)
+	{
+		if(!visited[i])
+			dfs(i,visited,AL,stk);
+	}
+	
+	while(!stk.empty())
+	{
+		cout<<stk.top()<<" ";
+		stk.pop();
+	}
+	return 0;
+}
